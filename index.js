a802e5d1a6bd5fd3e5d6b1b6011e1c93d5a10bbd
AnwardeenM <kalilanwar597@gmail.com>
2022-03-07 11:59:16 +0530

    trial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+const express = require("express");
+// const req = require("express/lib/request");
+
+const app = get(logger);
+
+app.use();
+
+// middlewares requires 3 arguments req,res,next()
+
+"logger"(req,res,next());
+
+CheckPermission(req,res,next());
+
